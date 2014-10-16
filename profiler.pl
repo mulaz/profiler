c2a32b3e9c2a2f9440c7c3adf06b7ee27644b997
mulaz <dmulac@gmail.com>
2014-10-16 17:19:27 +0200

    fix

--- profiler.pl
+++ profiler.pl
@@ -1,9 +1,13 @@
 #!/usr/bin/perl
 use Data::Dumper;
 use Sys::Hostname;
-
 $host = hostname;
 $time=time();
+$break=0;
+
+$SIG{INT} = sub { $break=1; };
+
+
 $filename=$host."_".$time.".log";
 open(my $fh, '>', $filename) or die "Could not open file '$filename' $!";
 
@@ -11,6 +15,10 @@ open(my $fh, '>', $filename) or die "Could not open file '$filename' $!";
 print $fh "#time,cpu_used_percentage,\n";
 
 while(1) {
+    if($break) { 
+	close $fh;
+	die "ctrl c\n"; }
+
     print $fh time();
     print $fh ",";
     print $fh getcpu();
@@ -18,21 +26,22 @@ while(1) {
 
 
     print $fh "\n";
-    exit;
-}
+    print "*";
+    sleep 1;
+} 
 
 
 
 
 sub getcpu() {
-    my $line=`top -b -n 1 | grep Cpu`;
+    my $line=`top -b -n 1 | head | grep Cpu`;
     my $used;
     if($line=~/ (\S+?) id/s) {
 	my $idle=$1;
 	$used=100-$idle;
     }
     else {
-	return "err";
+	return "-1";
     }
     return $used;
 
